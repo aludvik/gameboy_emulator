58de0a9b9a2eb6265032b5d9c756d0d1c117e1d2
Adam <rotestein@gmail.com>
2015-01-19 23:43:03 -0600

    Added struct for holding state of memory.

--- mem.h
+++ mem.h
@@ -1,14 +1,23 @@
 #ifndef MEM_H
 #define MEM_H
 
-#define MAX_ROM_SIZE 0x8000
+// Max ROM size is 2 MBytes
+#define MAX_ROM_SIZE 2*1024*1024
 #define RAM_SIZE 0xFFFF
 
 // *** Repeated typedef in cpu.h ***
 typedef unsigned int byte;
 typedef unsigned int word;
 
-void loadRom(char fn[], byte rom[]);
+typedef struct {
+    int cartridge_type, 
+    int rom_size, 
+    int bios_mapped
+} MEMState;
+
+extern const MEMState MEM_DEFAULT;
+
+int loadRom(char fn[]);
 void dissassembleRom(byte rom[]);
 
 void decode(byte *b, int i);
