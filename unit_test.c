8371664fed4eb71577cf3d0d6b9b9122087d878d
Adam <rotestein@gmail.com>
2015-01-19 23:40:03 -0600

    More testing.

--- unit_test.c
+++ unit_test.c
@@ -2,11 +2,36 @@
 
 #include "emu.h"
 
-void loadAndDissassemble();
+void memTest1();
+void memTest2();
+void cpuTest1();
 
 int main(int argc, char *argv) {
 
-    State cpu = STATE_DEFAULT;
+    memTest2();
+
+    return 0;
+}
+
+void memTest1() {
+    byte rom[MAX_ROM_SIZE];
+
+    loadRom("blue.gb");
+
+    dissassembleRom(rom);
+}
+
+void memTest2() {
+    char fn[30];
+
+    printf("Enter rom name: ");
+    scanf("%s", fn);
+    printf("ROM loaded was %d bytes.\n", loadRom(fn));
+
+} 
+
+void cpuTest1() {
+    CPUState cpu = CPU_DEFAULT;
 
     cpu.c = 0xFF;
 
@@ -15,14 +40,4 @@ int main(int argc, char *argv) {
     execute(&cpu);
 
     printf("%#06x\n", cpu.b);
-
-    return 0;
-}
-
-void loadAndDissassemble() {
-    byte rom[MAX_ROM_SIZE];
-
-    loadRom("blue.gb", rom);
-
-    dissassembleRom(rom);
 }
