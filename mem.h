000be6b38b91a4ce8c12215111e1c67d1db02031
Adam <rotestein@gmail.com>
2015-01-23 00:04:40 -0600

    Fixed MEMState. Added print. wb/ww returns success.

--- mem.h
+++ mem.h
@@ -1,17 +1,16 @@
 #ifndef MEM_H
 #define MEM_H
 
+#include "types.h"
+
 // Max ROM size is 2 MBytes
 #define MAX_ROM_SIZE 2*1024*1024
 #define RAM_SIZE 0xFFFF
 
-typedef unsigned int byte;
-typedef unsigned int word;
-
 typedef struct {
-    int cartridge_type, 
-    int rom_size, 
-    int bios_mapped
+    int cartridge_type;
+    int rom_size;
+    int bios_mapped;
 } MEMState;
 
 extern const MEMState MEM_DEFAULT;
@@ -21,10 +20,12 @@ void dissassembleRom(byte rom[]);
 
 void decode(byte *b, int i);
 
-void wb(word address, byte value);
+int wb(word address, byte value);
 byte rb(word address);
 
-void ww(word address, word value);
+int ww(word address, word value);
 byte rw(word address);
+
+void printMemory(byte start, byte end);
 
 #endif
